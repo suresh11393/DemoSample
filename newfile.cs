f39852cfb15a4a476d46b88941fc00fa3a7936f3
Suresh D <SureshD@gmail.com>
2018-07-14 16:33:18 +0530

    Trial branch

--- newfile.cs
+++ newfile.cs
@@ -1,8 +1,7 @@
-skjsdfhksf
+kjsdfhksf
 <<<<<<< HEAD
-sresh lunch
+lunch
 hello
 =======
 suresh lunch
-susu
->>>>>>> 87f5d55e8d589369ceee85194656606d4466232f
+>>>>> 87f5d55e8d589369ceee85194656606d4466232f
